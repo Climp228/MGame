3cb354df78bd372a2f8c337f8476941ec67e7f2c
Climp228 <climpser@gmail.com>
2024-08-20 19:03:16 +0300

    New SetUpGame method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace MGame
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGame();
+
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🦑","🦑",
+                "🐬","🐬",
+                "🦍","🦍",
+                "🙈","🙈",
+                "🐷","🐷",
+                "🐎","🐎",
+                "🦁","🦁",
+                "🐘","🐘",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
